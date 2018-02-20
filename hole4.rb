02b9150b03c09b6ee5398446ee1b468c87020f95
Bobby Caya <bob.caya1@gmail.com>
2018-02-20 09:30:09 -0700

    adds hole 4

--- hole4.rb
+++ hole4.rb
@@ -1,3 +1,22 @@
+require 'pry'
 def multiples
-  
+  puts "Enter a number"
+  n = gets.to_i 
+  p "enter max value"
+  m = gets.to_i
+  (n..m).each do |i|
+  if i % n == 0 
+  puts i 
+
+end  
 end 
+end 
+multiples 
+
+#The method takes in a number
+# and a max value then finds all 
+#multiples of the 
+#number up to the max value.
+# 1 takes in number 
+# 2 takes in max value
+# 3. finds all multiples up to max values
